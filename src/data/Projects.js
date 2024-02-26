bc905bae8c6422e9df731d7966e7a5770f786e85
abanchi <agustin.banchi@uosolutions.com>
2024-02-26 17:25:17 -0300

    actualizacion de projects.js se importan correctamente las imagenes para su visualizacion en el build

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,3 +1,7 @@
+import Veterinaria from "../assets/images/veterinaria.jpg";
+import Estancia from "../assets/images/avemaria.jpg";
+import Luli from "../assets/images/luli-sulichin.jpg";
+import Cepil from "../assets/images/cepil.jpg";
 import {
   SiBootstrap,
   SiCss3,
@@ -12,7 +16,7 @@ export const projects = [
     title: "Seguimiento Pacientes Veterinaria",
     description:
       "Trabajo practico de curso realizado en plataforma  UDEMY de seguimiento de pacientes para una veterinaria utilizando las tecnologias de React.js &Tailwind.css",
-    image: `src/assets/images/veterinaria.jpg`,
+    image: `${Veterinaria}`,
     code: "https://github.com/agubanchi/citas_react",
     link: "https://listado-veterinaria.netlify.app/",
     icon01_name: "React.Js",
@@ -24,7 +28,7 @@ export const projects = [
     title: "Estancia Ave Maria",
     description: `Diseño y desarrollo de sitio web  profesional para estancia ubicada en la ciudad de Tandil, provincia de Bs.As.
       Se trabajó en un rediseño completo de sitio, teniendo como prioridad una mejor experiencia para los usuarios.`,
-    image: `src/assets/images/avemaria.jpg`,
+    image: `${Estancia}`,
     link: "https://avemariatandil.com/",
     icon01_name: "Wordpress",
     icon02_name: "CSS3",
@@ -36,7 +40,7 @@ export const projects = [
     title: "Luli Sulichin - Portfolio Personal",
     description: `Diseño de sitio web institucional de arte para clienta en Miami, EE.UU.
     Su intención era un diseño super minimalista y limpio para dar a conocer sus obras y que estas tengan mayor visibilidad.`,
-    image: `src/assets/images/luli-sulichin.jpg`,
+    image: `${Luli}`,
     link: "https://lulisulichin.com/",
     icon01_name: "Wordpress",
     icon02_name: "CSS3",
@@ -48,7 +52,7 @@ export const projects = [
     title: "C.E.P.I.L - Centro de Psicologia Internacional Libedinsky",
     description: `Diseño y desarrollo de sitio web institucional “One-page” para familia de Psicólogos profesionales.
     El concepto de diseño era realizar algo simple y funcional`,
-    image: `src/assets/images/cepil.jpg`,
+    image: `${Cepil}`,
     link: "https://www.centrolibedinsky.com/",
     icon01_name: "HTML5",
     icon02_name: "Bootstrap",
